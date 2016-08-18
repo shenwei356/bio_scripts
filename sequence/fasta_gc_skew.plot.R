76713be380acb3a7bd4803407737e85f315832e3
shenwei356 <shenwei356@gmail.com>
2016-08-18 16:43:51 +0800

    update fasta_gc_skew.plot.R

--- sequence/fasta_gc_skew.plot.R
+++ sequence/fasta_gc_skew.plot.R
@@ -8,6 +8,44 @@ library(reshape2)
 library(scales)
 library(ggthemes)
 
+shenwei356.theme <-  theme_bw() +
+  theme(
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line.x = element_line(colour = "black", size = 0.8),
+    axis.line.y = element_line(colour = "black", size = 0.8),
+    axis.ticks.x = element_line(size = 0.8),
+    axis.ticks.y = element_line(size = 0.8),
+    #     axis.text.x = element_text(
+    #       angle = 30, hjust = 1, vjust = 1
+    #     ),
+    
+    legend.key = element_blank(),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 12, face = "plain"),
+    legend.background = element_rect(fill = "transparent"),
+    strip.background = element_rect(
+      colour = "white",
+      fill = "white",
+      size = 0.2
+    ),
+    strip.text.x = element_text(size = 16),
+    
+    text = element_text(
+      size = 14,
+      family = "arial",
+      face = "bold"
+    ),
+    plot.title = element_text(
+      size = 16,
+      family = "arial",
+      face = "bold"
+    )
+  )
+
+
 parser <-
   ArgumentParser(description = "Plot GC and GC Skew with the result produced by fasta_gc_skew.py",
                  formatter_class = "argparse.RawTextHelpFormatter")
@@ -17,26 +55,49 @@ parser$add_argument("infile", type = "character",
 parser$add_argument("outfile", type = "character",
                     help = "outfile")
 parser$add_argument(
-  "-xi", "--x-interval", type = "integer",
-  default = 1000000, help = "x axix interval [1,000,000]"
+  "-xi",
+  "--x-interval",
+  type = "integer",
+  default = 1000000,
+  help = "x axix interval [1,000,000]"
 )
-parser$add_argument("-n", type = "integer", default = 10,
+parser$add_argument("-n",
+                    type = "integer",
+                    default = 10,
                     help = "divide the normalized accum_gcskew by n so it looks better [10]")
 parser$add_argument(
-  "--width", metavar = "width", type = "integer", default = 20,
+  "--width",
+  metavar = "width",
+  type = "integer",
+  default = 20,
   help = "output image width [20]"
 )
 parser$add_argument(
-  "--height", metavar = "height", type = "integer", default = 5,
+  "--height",
+  metavar = "height",
+  type = "integer",
+  default = 5,
   help = "output image height [5]"
 )
-parser$add_argument("-g", "--gc-content", action = "store_true",
-  dest = "gc_content", help = "only plot GC Content")
-parser$add_argument("-s", "--gc-skew", action = "store_true",
-                       dest = "gc_skew", help = "only plot GC Skew")
 parser$add_argument(
-  "-t", "--title", metavar = "title", type = "character", 
-  default = "GC Content/GC Skew", help = "title"
+  "-g",
+  "--gc-content",
+  action = "store_true",
+  dest = "gc_content",
+  help = "only plot GC Content"
+)
+parser$add_argument("-s",
+                    "--gc-skew",
+                    action = "store_true",
+                    dest = "gc_skew",
+                    help = "only plot GC Skew")
+parser$add_argument(
+  "-t",
+  "--title",
+  metavar = "title",
+  type = "character",
+  default = "GC Content/GC Skew",
+  help = "title"
 )
 
 args <- parser$parse_args()
@@ -52,7 +113,7 @@ if (args$gc_content && !args$gc_skew) {
   df['gcskew'] = NULL
   df['accum_gcskew'] = NULL
 }
-if (! args$gc_content && args$gc_skew) {
+if (!args$gc_content && args$gc_skew) {
   df['gc'] = NULL
 }
 
@@ -60,30 +121,21 @@ df_m <- melt(df, id.vars = c("chr", "loc"))
 
 p <- ggplot(df_m) +
   geom_line(aes(loc, value, color = variable)) +
-  geom_hline(aes(yintercept = 0)) +
+  geom_hline(aes(yintercept = 0), linetype = 2) +
   scale_size(range = c(0.1)) +
-  scale_colour_wsj()+
+  scale_colour_stata() +
   facet_grid(chr ~ .) +
   ylab(NULL) +
-  xlab("Position(bp)") + 
-  scale_x_continuous(breaks = seq(0, max(df$loc), by = args$x_interval), labels = comma) +
+  xlab("Position(bp)") +
+  scale_x_continuous(breaks = seq(0, max(df$loc), by = args$x_interval),
+                     labels = comma) +
   ggtitle(args$title) +
-  theme_bw() +
-  theme(
-    panel.border = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    axis.line = element_line(colour = "black"),
-    legend.key = element_blank(),
-    legend.text=element_text(size=12),
-    legend.title = element_blank(),
-    legend.position = "bottom",
-    strip.background = element_rect(
-      colour = "white", fill = "white",
-      size = 0.2
-    )
-  )
+  shenwei356.theme +
+  theme(legend.position = "top")
 
 ggsave(
-  p, file = args$outfile, width = args$width, height = args$height
+  p,
+  file = args$outfile,
+  width = args$width,
+  height = args$height
 )
